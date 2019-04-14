36e57ec91123c97dd6d5c939053ca9d4892f329b
kianoosh <kianoosh.hooshmand@gmail.com>
2019-04-14 23:50:03 +0430

    footer get styled

--- App/MusicPlayer/GlobalFooter.js
+++ App/MusicPlayer/GlobalFooter.js
@@ -1,22 +1,46 @@
 import React from 'react'
-import {View} from 'react-native'
-import {Text, Button} from 'react-native-elements'
+import {StyleSheet, View} from 'react-native'
+import {Text, Card, Icon} from 'react-native-elements'
 
 
 class GlobalFooter extends React.Component {
     constructor(props, context) {
-        super(props)
-        this.state = {
-        }
+        super(props);
+        this.state = {}
     }
 
     render() {
-        return(
-            <View style={{backgroundColor: 'blue'}}>
-                <Text style={{color: 'white'}}>Music player here ;)</Text>
+        return (
+            <View style={styles.container}>
+                <Icon type={'foundation'} name={'previous'} size={styles.iconSize}  color={'white'} />
+                <Icon type={'foundation'} name={'play-circle'} size={styles.iconSize} containerStyle={styles.playButtonStyle} color={'white'}/>
+                <Icon type={'foundation'} name={'next'} size={styles.iconSize} containerStyle={styles.nextButtonStyle} color={'white'}/>
             </View>
         )
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        paddingTop: 10,
+        paddingBottom: 10,
+        backgroundColor: '#393e42'
+    },
+    playButtonStyle: {
+        marginRight: 20,
+        marginLeft: 20
+    },
+    nextButtonStyle: {
+        marginRight: 10
+    },
+    iconSize: 40
+});
+
 export default GlobalFooter
+
+// {/*<View style={{backgroundColor: 'blue'}}>*/}
+// {/*    <Text style={{color: 'white'}}>Music player here ;)</Text>*/}
+// {/*</View>*/}
